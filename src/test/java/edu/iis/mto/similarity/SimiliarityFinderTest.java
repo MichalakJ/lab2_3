47046b891e46cc1dd0905f00f4738b896b06dbd3
MichalakJ <daran.dasaner@gmail.com>
2016-04-14 18:10:12 +0200

    implemented test for testing behaviour

--- src/test/java/edu/iis/mto/similarity/SimiliarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimiliarityFinderTest.java
@@ -16,10 +16,12 @@ import org.junit.Test;
  */
 public class SimiliarityFinderTest {
     private SimilarityFinder finder;
+    SequenceSearcherDubler searcher;
     @Before
     public void setUpforTest(){
-        SequenceSearcherDubler searcher = new SequenceSearcherDubler();
+        searcher = new SequenceSearcherDubler();
         finder = new SimilarityFinder(searcher);
+        searcher.resetCounter();
     }
     @Test
     public void givenTwoEmptySequences_whenCalculateJackardSimiliarity_ThenResultIsOne(){
@@ -35,5 +37,12 @@ public class SimiliarityFinderTest {
         double result = finder.calculateJackardSimilarity(seq1, seq2);
         System.out.println(result);
         assertThat(result,equalTo(1.0/5.0));
+    }
+    @Test
+    public void givenSequenceWithThreeElements_whenCalculateJackardSimilaritiy_thenSearchCalledThreeTimes(){
+        int seq1[] = {1,2,3};
+        int seq2[] = {3,4,5};
+        double result = finder.calculateJackardSimilarity(seq1, seq2);
+        assertThat(searcher.getSearchCalled(), equalTo(3));
     }
 }
